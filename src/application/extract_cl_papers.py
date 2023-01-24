6bc35a0751014d7164b28afbec0d26812ab9f4d4
Ekaterina Borisova <89642052+esborisova@users.noreply.github.com>
2023-01-24 17:11:09 +0100

    typos fixed

--- src/application/extract_cl_papers.py
+++ src/application/extract_cl_papers.py
@@ -2,9 +2,9 @@
 
 import pandas as pd
 
-df = pd.read_json("data/arxiv-metadata-oai-snapshot.json", lines=True)
+df = pd.read_json("../../data/arxiv-metadata-oai-snapshot.json", lines=True)
 categories = df.categories.unique().tolist()
 print(len(df))
 labels = [category for category in categories if "cs.CL" in category]
 data = df.loc[df['categories'].isin(labels)]
-data.to_csv("data/CL_papers.csv", index=False)
+data.to_csv("../../data/CL_papers.csv", index=False)
---
+++ src/application/get_keywords.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import spacy
+import pytextrank
+
+
+data = pd.read_csv("../../data/CL_papers.csv")
+abstracts = data["abstract"].tolist()
+abstracts = [abstract for abstract in abstracts if abstract != ""]
+
+
+keywords_titles = pd.DataFrame(columns = ["title", "keywords", "ranks", "counts"])
+titles = data["title"].tolist()
+titles = [title for title in titles if title != ""]
+keywords_titles["title"] = titles
+
+nlp = spacy.load("en_core_web_sm")
+nlp.add_pipe("textrank")
+
+keywords = []
+ranks = []
+counts = []
+
+for title in titles:
+    doc = nlp(title)
+
+    title_keywords = []
+    title_ranks = []
+    title_counts = []
+    
+    for phrase in doc._.phrases:
+        title_keywords.append(phrase.text)
+        title_ranks.append(phrase.rank)
+        title_counts.append(phrase.count)
+    keywords.append(title_keywords)
+    ranks.append(title_ranks)
+    counts.append(title_counts)
+
+keywords_titles["keywords"] = keywords
+keywords_titles["ranks"] = ranks
+keywords_titles["counts"] = counts
+
+keywords_titles.to_csv("../../data/keywords_titles.csv", index=False)
