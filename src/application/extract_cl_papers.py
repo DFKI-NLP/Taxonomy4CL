85cd8fe270c0a40fa206b987f1aa90bacfd6b295
Ekaterina Borisova <89642052+esborisova@users.noreply.github.com>
2023-01-24 15:08:56 +0100

    1. pipeline to filter only CL papers 2. keywords per title with ranks and count

---
+++ src/application/extract_cl_papers.py
@@ -0,0 +1,10 @@
+"""Extract only CL papers from arXive data"""
+
+import pandas as pd
+
+df = pd.read_json("data/arxiv-metadata-oai-snapshot.json", lines=True)
+categories = df.categories.unique().tolist()
+print(len(df))
+labels = [category for category in categories if "cs.CL" in category]
+data = df.loc[df['categories'].isin(labels)]
+data.to_csv("data/CL_papers.csv", index=False)
