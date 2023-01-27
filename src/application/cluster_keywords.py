0dc1fdbce8bd846083d856ba4d1f800642d6defd
Ekaterina Borisova <89642052+esborisova@users.noreply.github.com>
2023-01-27 14:18:21 +0100

    1. pipeline for extracting both titles and abstracts 2. clustering titles

---
+++ src/application/cluster_keywords.py
@@ -0,0 +1,22 @@
+"""Cluster keywords based on embeddings"""
+import pandas as pd
+from sentence_transformers import SentenceTransformer, util
+import ast
+
+data = pd.read_csv("../../data/keywords_titles.csv")
+data["keywords"] = data["keywords"].apply(lambda x: ast.literal_eval(x))
+keywords = data["keywords"][:100].tolist()
+keywords = [keyword for doc in keywords for keyword in doc]
+
+model = SentenceTransformer("sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+corpus_embeddings = model.encode(keywords, batch_size=64, show_progress_bar=True, convert_to_tensor=True)
+
+min_community_size = 10
+min_community_size = min(min_community_size, len(corpus_embeddings))
+clusters = util.community_detection(corpus_embeddings, min_community_size=min_community_size, threshold=0.75)
+
+for i, cluster in enumerate(clusters):
+    print("\nCluster {}, #{} Elements ".format(i+1, len(cluster)))
+    for sentence_id in cluster[0:max]:
+        print("\t", keywords[sentence_id])
+    print("\t", "...")
--- src/application/get_keywords.py
+++ src/application/get_keywords.py
@@ -1,39 +1,32 @@
+"""Extract keywords from titles and abstracts using textrank"""
 import pandas as pd
 import spacy
 import pytextrank
 
-
-data = pd.read_csv("../../data/CL_papers.csv")
-
-keywords_titles = pd.DataFrame(columns = ["title", "keywords", "ranks", "counts"])
-titles = data["title"].tolist()
-titles = [title for title in titles if title != ""]
-keywords_titles["title"] = titles
-
 nlp = spacy.load("en_core_web_sm")
 nlp.add_pipe("textrank")
+
+data = pd.read_csv("../../data/CL_papers.csv")
+labels = ["title", "abstract"]
 
 keywords = []
 ranks = []
 counts = []
 
-for title in titles:
-    doc = nlp(title)
-
-    title_keywords = []
-    title_ranks = []
-    title_counts = []
+for label in labels:
+    df_keywords = pd.DataFrame(columns = [label, "keywords"])
+    docs = data[label].tolist()
+    docs = [doc for doc in docs if doc != ""]
+    df_keywords[label] = docs
     
-    for phrase in doc._.phrases:
-        title_keywords.append(phrase.text)
-        title_ranks.append(phrase.rank)
-        title_counts.append(phrase.count)
-    keywords.append(title_keywords)
-    ranks.append(title_ranks)
-    counts.append(title_counts)
+    for text in docs:
+        doc = nlp(text)
+        keywords_doc = []
+        
+        for phrase in doc._.phrases:
+            keywords_doc.append(phrase.text)
+        keywords.append(keywords_doc)
+    
+keywords["keywords"] = keywords
 
-keywords_titles["keywords"] = keywords
-keywords_titles["ranks"] = ranks
-keywords_titles["counts"] = counts
-
-keywords_titles.to_csv("../../data/keywords_titles.csv", index=False)
+keywords.to_csv(f"../../data/keywords_{label}s.csv", index=False)
