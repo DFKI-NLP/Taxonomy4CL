da83eddfdce716c3f8a61bc8d3ec94bc3e8f209b
Ekaterina Borisova <89642052+esborisova@users.noreply.github.com>
2023-01-27 16:05:30 +0100

    typos fixed, keywords files updated

--- src/application/get_keywords.py
+++ src/application/get_keywords.py
@@ -9,24 +9,23 @@ nlp.add_pipe("textrank")
 data = pd.read_csv("../../data/CL_papers.csv")
 labels = ["title", "abstract"]
 
-keywords = []
-ranks = []
-counts = []
 
 for label in labels:
     df_keywords = pd.DataFrame(columns = [label, "keywords"])
+  
+    keywords = []
+
     docs = data[label].tolist()
     docs = [doc for doc in docs if doc != ""]
     df_keywords[label] = docs
     
     for text in docs:
         doc = nlp(text)
-        keywords_doc = []
         
+        keywords_doc = []
         for phrase in doc._.phrases:
             keywords_doc.append(phrase.text)
         keywords.append(keywords_doc)
     
-keywords["keywords"] = keywords
-
-keywords.to_csv(f"../../data/keywords_{label}s.csv", index=False)
+    df_keywords["keywords"] = keywords
+    df_keywords.to_csv(f"../../data/keywords_{label}s.csv", index=False)
